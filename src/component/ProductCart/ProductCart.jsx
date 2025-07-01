157a020cabd671b1ec6dc28a9e4bc3e933ff63ae
Sharif101 <sharif009009@gmail.com>
2025-07-01 15:47:07 +0600

    func: update quantity cart product

--- src/component/ProductCart/ProductCart.jsx
+++ src/component/ProductCart/ProductCart.jsx
@@ -1,61 +1,62 @@
 "use client";
 
-import React from "react";
+import React, { useContext } from "react";
 import Image from "next/image";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { Cart } from "@/contexts/context";
 
 export default function ProductCart() {
-  const cartItems = [
-    {
-      id: 1,
-      shop: "BD FASHION HOUSE",
-      name: "Bestway Brand Air Inflatable 5 In 1 semi Double Sofa",
-      image:
-        "http://157.230.240.97:8888/storage/media/20250625_191610_8408d9d2-d647-4c43-8e47-9de462039ae2.jpg",
-      color: "red",
-      size: "M",
-      price: 11139,
-      originalPrice: 1539,
-      quantity: 2,
-    },
-    {
-      id: 2,
-      shop: "BD FASHION HOUSE",
-      name: "Bestway Brand Air Inflatable 5 In 1 semi Double Sofa",
-      image:
-        "http://157.230.240.97:8888/storage/media/20250625_191610_8408d9d2-d647-4c43-8e47-9de462039ae2.jpg",
-      color: "red",
-      size: "M",
-      price: 11139,
-      originalPrice: 1539,
-      quantity: 2,
-    },
-  ];
+  const { stateCart, dispatchCart } = useContext(Cart);
+  const cartProducts = stateCart.info;
+
+  const subTotal = cartProducts.reduce((sum, item) => {
+    const qty = item.quantity || 1;
+    return sum + item.discount_price * qty;
+  }, 0);
+
+  const handleCart = (action, id) => {
+    const updatedCart = cartProducts.map((item) => {
+      if (item.id === id) {
+        const qty = item.quantity ?? 1;
+        const newQty =
+          action === "increase"
+            ? qty + 1
+            : action === "decrease"
+            ? Math.max(1, qty - 1)
+            : qty;
+
+        return { ...item, quantity: newQty };
+      }
+      return item;
+    });
+
+    dispatchCart({ type: "set", payload: updatedCart });
+  };
 
   return (
     <div className="px-20 py-8 bg-gray-50 min-h-screen">
       <div className="text-sm text-gray-500 mb-4">Home &gt; My Cart</div>
 
       <div className="flex flex-col md:flex-row gap-8">
-        {/* Cart Items */}
         <div className="flex-1 space-y-4">
           <div className="bg-white flex justify-between items-center p-4 rounded">
             <h1 className="text-2xl font-bold text-[#0F172A]">
-              My Cart ({cartItems.length})
+              My Cart ({cartProducts.length})
             </h1>
             <p className="text-[#475569] text-[14px] cursor-pointer">
               Clear All
             </p>
           </div>
+
           <div className="bg-white p-4 rounded-lg shadow">
-            {cartItems.map((item, i) => (
+            {cartProducts.map((item, i) => (
               <div key={i} className="flex items-start gap-4 mb-10">
                 <Image
                   src={item.image}
                   alt={item.name}
                   width={80}
                   height={80}
-                  className="rounded object-cover"
+                  className="rounded object-cover w-[80px] h-[80px]"
                 />
 
                 <div className="flex-1">
@@ -65,27 +66,39 @@ export default function ProductCart() {
                     </h2>
                     <div className="text-right">
                       <span className="text-[#0F172A] font-bold text-lg mr-3">
-                        ৳{item.price}
+                        ৳{item.discount_price * (item.quantity || 1)}
                       </span>
                       <span className="line-through text-[#475569] text-sm">
-                        ৳{item.originalPrice}
+                        ৳{item.regular_price}
                       </span>
                     </div>
                   </div>
-                  <p className="text-sm text-gray-500 mt-1">
-                    Color: {item.color}; Size: {item.size}
-                  </p>
+
+                  {item.is_variant &&
+                    item.variation_attributes?.map((attr, i) => (
+                      <div key={i} className="mt-1">
+                        <div className="text-sm text-[#0F172A] flex gap-2">
+                          <span>{attr.attribute?.name}</span>
+                          <span>:</span>
+                          <span>{attr.attribute_option?.attribute_value}</span>
+                        </div>
+                      </div>
+                    ))}
 
                   <div className="mb-6 mt-3 flex items-center gap-5">
                     <div className="flex items-center justify-between border border-gray-200 rounded-full w-fit px-1 py-1">
-                      <button className="w-8 h-8 rounded-full bg-gray-100 text-gray-600 text-xl flex items-center justify-center cursor-pointer hover:bg-gray-200 transition">
+                      <button
+                        onClick={() => handleCart("decrease", item.id)}
+                        className="w-8 h-8 rounded-full bg-gray-100 text-gray-600 text-xl flex items-center justify-center cursor-pointer hover:bg-gray-200 transition"
+                      >
                         -
                       </button>
                       <div className="w-10 text-center text-black text-[14px]">
-                        0
+                        {item.quantity || 1}
                       </div>
                       <button
-                        className={`w-8 h-8 rounded-full bg-gray-100 text-gray-600 text-xl flex items-center justify-center transition cursor-pointer`}
+                        onClick={() => handleCart("increase", item.id)}
+                        className="w-8 h-8 rounded-full bg-gray-100 text-gray-600 text-xl flex items-center justify-center transition cursor-pointer"
                       >
                         +
                       </button>
@@ -100,13 +113,16 @@ export default function ProductCart() {
           </div>
         </div>
 
-        {/* Order Summary */}
         <div className="w-full md:w-[380px] h-fit bg-white p-4 rounded-lg shadow">
           <h2 className="font-semibold mb-4">Order summary</h2>
+
           <div className="flex justify-between text-sm mb-2">
-            <p className="text-[#475569]">Price ({cartItems.length} items)</p>
-            <p>৳00</p>
+            <p className="text-[#475569]">
+              Price ({cartProducts.length} items)
+            </p>
+            <p>৳{subTotal}</p>
           </div>
+
           <div className="flex justify-between text-sm mb-2">
             <p className="text-[#475569]">Shipping fee</p>
             <p className="text-blue-500 text-xs cursor-pointer">To be added</p>
@@ -125,7 +141,7 @@ export default function ProductCart() {
 
           <div className="flex justify-between mt-4 font-bold">
             <p className="text-[#334155]">Sub Total</p>
-            <p>৳00</p>
+            <p>৳{subTotal}</p>
           </div>
 
           <button className="w-full bg-emerald-600 text-white py-2 mt-4 rounded hover:bg-emerald-700">
